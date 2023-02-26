f8b0f0f99631289d6d6080e385779f3621bae6b2
Rizqi Nur Andi Putra <92218072+Thecaracter@users.noreply.github.com>
2023-02-26 22:37:11 +0700

    layout selesai

--- lib/calculator.dart
+++ lib/calculator.dart
@@ -1,19 +1,17 @@
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
-import 'package:flutter/src/widgets/container.dart';
-import 'package:flutter/src/widgets/framework.dart';
 
-class calculator extends StatefulWidget {
-  const calculator({super.key});
+class Calculator extends StatefulWidget {
+  const Calculator({Key? key}) : super(key: key);
 
   @override
-  State<calculator> createState() => _calculatorState();
+  _CalculatorState createState() => _CalculatorState();
 }
 
-class _calculatorState extends State<calculator> {
-  String userinput = '';
+class _CalculatorState extends State<Calculator> {
+  String userInput = '';
   String result = '0';
-  List<String> Listtombol = [
+  List<String> buttonList = [
     "AC",
     "(",
     ")",
@@ -35,65 +33,64 @@ class _calculatorState extends State<calculator> {
     ".",
     "="
   ];
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       backgroundColor: Color(0xFF1d2530),
-      body: Column(
-        children: [
-          SizedBox(
-            height: MediaQuery.of(context).size.height / 2,
-            // width: MediaQuery.of(context).size.width,
-            child: Column(
-              mainAxisAlignment: MainAxisAlignment.end,
-              children: [
-                Container(
-                  // color: Colors.white,
-                  padding: EdgeInsets.all(20),
-                  alignment: Alignment.centerRight,
-                  // height: 200,
-                  child: Text(
-                    userinput,
-                    style: TextStyle(
-                      fontSize: 48,
-                      color: Colors.white,
+      body: SingleChildScrollView(
+        child: Column(
+          children: [
+            SizedBox(
+              height: MediaQuery.of(context).size.height / 1,
+              child: Column(
+                mainAxisAlignment: MainAxisAlignment.end,
+                children: [
+                  Container(
+                    padding: EdgeInsets.all(20),
+                    alignment: Alignment.centerRight,
+                    child: Text(
+                      userInput,
+                      style: TextStyle(
+                        fontSize: 48,
+                        color: Colors.white,
+                      ),
                     ),
                   ),
-                ),
-                Container(
-                  // color: Colors.white,
-                  padding: EdgeInsets.all(10),
-                  alignment: Alignment.centerRight,
-                  child: Text(
-                    result,
-                    style: TextStyle(
-                      fontSize: 48,
-                      color: Colors.white,
+                  Container(
+                    padding: EdgeInsets.all(10),
+                    alignment: Alignment.centerRight,
+                    child: Text(
+                      result,
+                      style: TextStyle(
+                        fontSize: 48,
+                        color: Colors.white,
+                      ),
                     ),
                   ),
-                ),
-                Divider(
-                  color: Colors.white,
-                ),
-                Expanded(
-                    child: Container(
-                  padding: EdgeInsets.all(10),
-                  //
-                  child: GridView.builder(
-                      itemCount: Listtombol.length,
+                  Divider(
+                    color: Colors.white,
+                  ),
+                  Container(
+                    padding: EdgeInsets.all(10),
+                    child: GridView.builder(
+                      shrinkWrap: true,
+                      itemCount: buttonList.length,
                       gridDelegate: SliverGridDelegateWithFixedCrossAxisCount(
                         crossAxisCount: 4,
                         crossAxisSpacing: 12,
                         mainAxisSpacing: 12,
                       ),
                       itemBuilder: (BuildContext context, int index) {
-                        return CustomButton(Listtombol[index]);
-                      }),
-                ))
-              ],
+                        return CustomButton(buttonList[index]);
+                      },
+                    ),
+                  ),
+                ],
+              ),
             ),
-          )
-        ],
+          ],
+        ),
       ),
     );
   }
@@ -116,14 +113,15 @@ class _calculatorState extends State<calculator> {
           ],
         ),
         child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
           children: [
             Center(
               child: Text(
                 buttonText,
                 style: TextStyle(
-                  fontSize: 24,
-                  color: Colors.black,
-                ),
+                    fontSize: 24,
+                    color: Colors.black,
+                    fontWeight: FontWeight.bold),
               ),
             ),
           ],
